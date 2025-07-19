35e1c4425189a63d63e494c4fa00b42a08b39d0f
Prohor722 <prohor.b722@gmail.com>
2025-07-19 23:48:54 +0600

    buttons page added

---
+++ app/button/page.tsx
@@ -0,0 +1,12 @@
+//use client
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl bg-gray-300 font-bold text-center p-5'>Buttons</h1>
+    </div>
+  )
+}
+
+export default page
