81dfe3ae3eaa5c0be2de9b1f0258e125af9fd282
Prohor722 <prohor.b722@gmail.com>
2025-08-01 23:35:46 +0600

    another pagination ui added

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -4,8 +4,9 @@ import { useState } from "react";
 const PaginationPage = () => {
   const [current, setCurrent] = useState(1);
   const [active, setActive] = useState(1);
-  const total = 5;
+  const [selected, setSelected] = useState(1);
   const [page, setPage] = useState(1);
+  const total = 5;
   const totalPages = 10;
 
   const prev = () => page > 1 && setPage(page - 1);
@@ -84,6 +85,23 @@ const PaginationPage = () => {
             }`}
           >
             {i + 1}
+          </button>
+        ))}
+      </div>
+
+      {/* Pagination 5  */}
+      <div className="flex justify-center mt-10 space-x-2">
+        {[1, 2, 3, 4, 5].map((n) => (
+          <button
+            key={n}
+            onClick={() => setSelected(n)}
+            className={`w-10 h-10 rounded-md transition-all duration-300 ease-in-out transform font-semibold text-sm ${
+              selected === n
+                ? "bg-indigo-600 text-white shadow-xl shadow-indigo-300 scale-105"
+                : "bg-white text-gray-800 border border-gray-300 hover:bg-indigo-50"
+            }`}
+          >
+            {n}
           </button>
         ))}
       </div>
