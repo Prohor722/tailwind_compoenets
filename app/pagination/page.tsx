d0ed308a567fba581a4b3cd39d6d635589c3de49
Prohor722 <prohor.b722@gmail.com>
2025-08-01 23:38:51 +0600

    another pagination ui added

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -7,6 +7,8 @@ const PaginationPage = () => {
   const [selected, setSelected] = useState(1);
   const [page, setPage] = useState(1);
   const [step, setStep] = useState(1);
+  
+  const pages = [1, 2, 3, 4, 5];
   const total = 5;
   const totalPages = 10;
 
@@ -135,6 +137,24 @@ const PaginationPage = () => {
             }`}
           >
             {n}
+          </button>
+        ))}
+      </div>
+
+      {/* Pagination 8  */}
+      <div className="flex justify-center gap-6 mt-10 border-b border-gray-200">
+        {pages.map((n) => (
+          <button
+            key={n}
+            onClick={() => setPage(n)}
+            className={`pb-2 text-sm font-medium transition-all duration-300 relative ${
+              page === n ? "text-blue-600" : "text-gray-500 hover:text-blue-500"
+            }`}
+          >
+            {n}
+            {page === n && (
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-blue-600 rounded-full animate-[slide_0.3s_ease-in-out]"></span>
+            )}
           </button>
         ))}
       </div>
