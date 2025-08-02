52f80e828b45c59cef71466b342efc222de28603
Prohor722 <prohor.b722@gmail.com>
2025-08-02 20:39:01 +0600

    another pagination added

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -214,7 +214,34 @@ const PaginationPage = () => {
         ))}
       </div>
 
-
+      {/* Pagination 12  */}
+      <div className="flex justify-center items-center gap-2 mt-10">
+        <button
+          onClick={() => setPage((p) => Math.max(p - 1, 1))}
+          className="px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100"
+        >
+          Prev
+        </button>
+        {[...Array(totalPages)].map((_, i) => (
+          <button
+            key={i}
+            onClick={() => setPage(i + 1)}
+            className={`px-3 py-2 rounded transition-all text-sm font-medium ${
+              page === i + 1
+                ? "bg-blue-600 text-white shadow"
+                : "text-gray-700 hover:bg-blue-50"
+            }`}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
+          className="px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
