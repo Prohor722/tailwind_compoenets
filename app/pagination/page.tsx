1ee960f8a95963320dc4eb170fa06dc722e2db08
Prohor722 <prohor.b722@gmail.com>
2025-08-05 23:15:15 +0600

    another pagination added

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -577,6 +577,24 @@ const PaginationPage = () => {
           <ArrowRight className="w-4 h-4" />
         </button>
       </div>
+
+      {/* Pagination 24  */}
+      <div className="flex justify-center mt-12 bg-white/20 backdrop-blur-md rounded-lg px-6 py-4 shadow-inner gap-3">
+      {[1, 2, 3, 4, 5].map((n) => (
+        <button
+          key={n}
+          onClick={() => setSelected(n)}
+          className={`px-4 py-2 rounded-lg font-medium text-sm transition-all duration-300 
+            ${
+              selected === n
+                ? ' text-blue-200 shadow-lg ring-2 ring-blue-300'
+                : 'text-white hover:bg-white/20'
+            }`}
+        >
+          {n}
+        </button>
+      ))}
+    </div>
     </div>
   );
 };
