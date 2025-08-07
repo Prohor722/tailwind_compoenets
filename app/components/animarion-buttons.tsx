38d90eb3ab9571ceef6166148e5dd142e3dd4ae7
Prohor722 <prohor.b722@gmail.com>
2025-08-07 22:37:05 +0600

    another button added

--- app/components/animarion-buttons.tsx
+++ app/components/animarion-buttons.tsx
@@ -156,6 +156,13 @@ export const AnimarionButtons = () => {
             Slide Up
           </span>
         </button>
+
+        <button className="relative px-5 py-2 text-blue-600 font-medium group">
+          Hover Me
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-600 transition-all group-hover:w-full duration-300"></span>
+        </button>
+
+        
       </div>
     </div>
   );
