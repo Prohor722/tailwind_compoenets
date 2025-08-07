bff13c527c73181e4d5e5c4efc9888bd6cde648f
Prohor722 <prohor.b722@gmail.com>
2025-08-07 22:40:20 +0600

    another button added

--- app/components/animarion-buttons.tsx
+++ app/components/animarion-buttons.tsx
@@ -179,6 +179,11 @@ export const AnimarionButtons = () => {
             Wipe Reveal
           </span>
         </button>
+
+        <button className="relative overflow-hidden px-6 py-3 bg-emerald-600 text-white rounded-lg group">
+  Ripple Hover
+  <span className="absolute top-1/2 left-1/2 w-0 h-0 bg-white/30 rounded-full transform -translate-x-1/2 -translate-y-1/2 group-hover:w-48 group-hover:h-48 transition-all duration-500 ease-out"></span>
+</button>
       </div>
     </div>
   );
