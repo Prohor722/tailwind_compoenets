123bacd222a4b394df6127d9f499a6b27c393bcf
Prohor722 <prohor.b722@gmail.com>
2025-08-08 22:13:12 +0600

    another button added

--- app/components/animarion-buttons.tsx
+++ app/components/animarion-buttons.tsx
@@ -226,7 +226,10 @@ export const AnimarionButtons = () => {
           <span className="relative z-10">Spin Border</span>
         </button>
 
-        
+        <button className="relative px-6 py-3 bg-yellow-500 text-black font-bold rounded-lg overflow-hidden group">
+          <span className="absolute inset-0 translate-x-[-100%] bg-yellow-300 skew-x-[-20deg] group-hover:translate-x-[100%] transition-transform duration-500"></span>
+          <span className="relative z-10">Lightning</span>
+        </button>
       </div>
     </div>
   );
