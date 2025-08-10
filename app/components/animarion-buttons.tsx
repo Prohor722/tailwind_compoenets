f5cc37cc831b99413d8872da56b8892a4177d87d
Prohor722 <prohor.b722@gmail.com>
2025-08-10 23:56:35 +0600

    another button added

--- app/components/animarion-buttons.tsx
+++ app/components/animarion-buttons.tsx
@@ -250,6 +250,10 @@ export const AnimarionButtons = () => {
         <button className="px-6 py-3 border border-cyan-400 text-cyan-400 hover:font-bold hover:text-white rounded-lg hover:bg-cyan-400 transition-all duration-300 hover:shadow-[0_0_20px_rgba(0,255,255,0.7)]">
           Ghost Glow
         </button>
+
+        <button className="px-6 py-3 font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:translate-y-1 hover:shadow-lg transition-transform duration-300">
+      children
+    </button>
       </div>
     </div>
   );
