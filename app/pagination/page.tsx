e81577e3fd6cb999bfc4bc108d46fcfc53dbf28d
Prohor722 <prohor.b722@gmail.com>
2025-08-27 00:42:20 +0600

    pagination4 destructured

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -7,6 +7,7 @@ import {
 import Pagination1 from "./components/Pagination1";
 import Pagination2 from "./components/Pagination2";
 import Pagination3 from "./components/Pagination3";
+import Pagination4 from "./components/Pagination4";
 
 
 const PaginationPage = () => {
@@ -158,21 +159,7 @@ const PaginationPage = () => {
       <Pagination3 />
 
       {/* Pagination 4  */}
-      <div className="flex justify-center mt-10 space-x-2">
-        {Array.from({ length: total }).map((_, i) => (
-          <button
-            key={i}
-            onClick={() => setActive(i + 1)}
-            className={`relative w-10 h-10 rounded-full border-2 flex items-center justify-center font-medium text-sm transition-all duration-300
-            ${active === i + 1
-                ? "border-blue-600 text-blue-600 bg-blue-100 ring-2 ring-blue-300"
-                : "border-gray-300 text-gray-700 hover:border-blue-400 hover:text-blue-500"
-              }`}
-          >
-            {i + 1}
-          </button>
-        ))}
-      </div>
+      <Pagination4 />
 
       {/* Pagination 5  */}
       <div className="flex justify-center mt-10 space-x-2">
