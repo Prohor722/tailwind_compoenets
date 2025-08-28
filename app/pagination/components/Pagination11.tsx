bd74bf449bfa7dda2437ebaf67e359e19c6b0c00
Prohor722 <prohor.b722@gmail.com>
2025-08-28 18:18:36 +0600

    pagination11 destructured

---
+++ app/pagination/components/Pagination11.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const Pagination11 = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 5;
+  
+  return (
+    <div className="flex justify-center gap-3 mt-10">
+        {[...Array(totalPages)].map((_, i) => (
+          <button
+            key={i}
+            onClick={() => setCurrentPage(i + 1)}
+            className={`px-4 py-2 rounded-md text-sm font-semibold transition-all duration-300 relative group
+            ${currentPage === i + 1
+                ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-lg"
+                : "bg-gray-100 text-gray-700 hover:text-blue-600"
+              }
+          `}
+          >
+            {i + 1}
+            <span className="absolute inset-0 rounded-md group-hover:ring-2 group-hover:ring-purple-400 group-hover:scale-105 transition-transform" />
+          </button>
+        ))}
+      </div>
+  )
+}
+
+export default Pagination11
--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -14,6 +14,7 @@ import Pagination7 from "./components/Pagination7";
 import Pagination8 from "./components/Pagination8";
 import Pagination9 from "./components/Pagination9";
 import Pagination10 from "./components/Pagination10";
+import Pagination11 from "./components/Pagination11";
 
 
 const PaginationPage = () => {
@@ -184,23 +185,7 @@ const PaginationPage = () => {
       <Pagination10 />
 
       {/* Pagination 11  */}
-      <div className="flex justify-center gap-3 mt-10">
-        {[...Array(totalPages)].map((_, i) => (
-          <button
-            key={i}
-            onClick={() => setCurrentPage(i + 1)}
-            className={`px-4 py-2 rounded-md text-sm font-semibold transition-all duration-300 relative group
-            ${currentPage === i + 1
-                ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-lg"
-                : "bg-gray-100 text-gray-700 hover:text-blue-600"
-              }
-          `}
-          >
-            {i + 1}
-            <span className="absolute inset-0 rounded-md group-hover:ring-2 group-hover:ring-purple-400 group-hover:scale-105 transition-transform" />
-          </button>
-        ))}
-      </div>
+      <Pagination11 />
 
       {/* Pagination 12  */}
       <div className="flex justify-center items-center gap-2 mt-10">
