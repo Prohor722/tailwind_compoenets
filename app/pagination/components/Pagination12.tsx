b7ae368fa5e88efd0ac188ec0625a9556b2857bd
Prohor722 <prohor.b722@gmail.com>
2025-08-28 18:28:15 +0600

    pagination12 some design updated

--- app/pagination/components/Pagination12.tsx
+++ app/pagination/components/Pagination12.tsx
@@ -8,7 +8,8 @@ const Pagination12 = () => {
     <div className="flex justify-center items-center gap-2 mt-10" title='Pagination-12'>
         <button
           onClick={() => setPage((p) => Math.max(p - 1, 1))}
-          className="cursor-pointer px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100"
+          className={`px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100
+            ${page===1? "cursor-not-allowed":"cursor-pointer"}`}
         >
           Prev
         </button>
@@ -17,8 +18,8 @@ const Pagination12 = () => {
             key={i}
             onClick={() => setPage(i + 1)}
             className={`px-3 py-2 rounded transition-all text-sm font-medium ${page === i + 1
-              ? "bg-blue-600 text-white shadow"
-              : "text-gray-700 hover:bg-blue-50"
+              ? "bg-blue-600 text-white shadow cursor-none"
+              : "text-gray-700 hover:bg-blue-50 cursor-pointer "
               }`}
           >
             {i + 1}
@@ -26,7 +27,8 @@ const Pagination12 = () => {
         ))}
         <button
           onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
-          className="px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100"
+          className={`px-3 py-2 text-sm text-gray-600 bg-gray-100 rounded hover:bg-blue-100
+            ${page===5? "cursor-not-allowed": "cursor-pointer"}`}
         >
           Next
         </button>
