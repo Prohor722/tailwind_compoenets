7a4526d326f0ae9634ff7321135538566e1a2de9
Prohor722 <prohor.b722@gmail.com>
2025-08-29 19:43:38 +0600

    pagination15 destructured

---
+++ app/pagination/components/Pagination15.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const Pagination15 = () => {
+  const [active, setActive] = useState(1);
+  const pages = [1, 2, 3, 4, 5];
+  
+  return (
+    <div className="flex justify-center mt-6 space-x-2">
+        {pages.map((page) => (
+          <button
+            key={page}
+            onClick={() => setActive(page)}
+            className={`px-4 py-2 rounded-full text-sm font-medium transition-all duration-300 ${page === active
+              ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-md"
+              : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+              }`}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
+  )
+}
+
+export default Pagination15
--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -18,6 +18,7 @@ import Pagination11 from "./components/Pagination11";
 import Pagination12 from "./components/Pagination12";
 import Pagination13 from "./components/Pagination13";
 import Pagination14 from "./components/Pagination14";
+import Pagination15 from "./components/Pagination15";
 
 
 const PaginationPage = () => {
@@ -200,20 +201,7 @@ const PaginationPage = () => {
       <Pagination14 />
 
       {/* Pagination 15  */}
-      <div className="flex justify-center mt-6 space-x-2">
-        {pages.map((page) => (
-          <button
-            key={page}
-            onClick={() => setActive(page)}
-            className={`px-4 py-2 rounded-full text-sm font-medium transition-all duration-300 ${page === active
-              ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-md"
-              : "bg-gray-100 text-gray-700 hover:bg-gray-200"
-              }`}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
+      <Pagination15 />
 
       {/* Pagination 16  */}
       <div className="flex justify-center mt-6 items-center gap-2">
@@ -286,20 +274,6 @@ const PaginationPage = () => {
             {page}
           </button>
         ))}
-
-        {/* <button className="px-2 py-1 bg-gray-100 rounded hover:bg-gray-200">
-          2
-        </button>
-        <span className="text-gray-500">...</span>
-        <button className="px-2 py-1 bg-blue-500 text-white rounded shadow hover:bg-blue-600">
-          8
-        </button>
-        <button className="px-2 py-1 bg-gray-100 rounded hover:bg-gray-200">
-          9
-        </button>
-        <button className="px-2 py-1 bg-gray-100 rounded hover:bg-gray-200">
-          10
-        </button> */}
       </div>
 
       {/* Pagination 18  */}
