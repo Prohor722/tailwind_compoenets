6bbc4b3c6066e11b0f4e4ad9d6713d57785467a1
Prohor722 <prohor.b722@gmail.com>
2025-08-30 20:15:01 +0600

    pagination23 modified

--- app/pagination/components/Pagination23.tsx
+++ app/pagination/components/Pagination23.tsx
@@ -10,7 +10,10 @@ const Pagination23 = () => {
         title='Pagination-23'>
         <button
           onClick={() => setPage((p) => Math.max(p - 1, 1))}
-          className="flex items-center gap-1 px-3 py-2 rounded-md text-gray-700 bg-gray-100 hover:bg-blue-100 transition"
+          className={`flex items-center gap-1 px-3 py-2 rounded-md 
+            text-gray-700 bg-gray-100 hover:bg-blue-100 transition
+            ${page === 1 ? "opacity-50 cursor-not-allowed" : "opacity-100 cursor-pointer"}
+            `}
         >
           <ArrowLeft className="w-4 h-4" />
           Prev
@@ -21,8 +24,8 @@ const Pagination23 = () => {
             key={i}
             onClick={() => setPage(i + 1)}
             className={`w-9 h-9 rounded-full flex items-center justify-center transition-all duration-300 text-sm font-medium ${page === i + 1
-              ? "bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-lg"
-              : "bg-white border border-gray-300 text-gray-700 hover:bg-blue-50"
+              ? "bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-lg cursor-not-allowed"
+              : "bg-white border border-gray-300 text-gray-700 hover:bg-blue-50 cursor-pointer"
               }`}
           >
             {i + 1}
@@ -31,7 +34,10 @@ const Pagination23 = () => {
 
         <button
           onClick={() => setPage((p) => Math.min(p + 1, total))}
-          className="flex items-center gap-1 px-3 py-2 rounded-md text-gray-700 bg-gray-100 hover:bg-blue-100 transition"
+          className={`flex items-center gap-1 px-3 py-2 rounded-md 
+            text-gray-700 bg-gray-100 hover:bg-blue-100 transition
+            ${page === total ? "opacity-50 cursor-not-allowed" : "opacity-100 cursor-pointer"}
+            `}
         >
           Next
           <ArrowRight className="w-4 h-4" />
