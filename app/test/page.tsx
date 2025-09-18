c027e69aae6fc80e2e42f0d45e76fc825910039e
Prohor722 <prohor.b722@gmail.com>
2025-09-18 21:35:29 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import FlipCard from "../card/components/FlipCard";
+import PulseGlowCard from "../card/components/PulseGlowCard";
 
 const page = () => {
 
   return (
     <div>
-      <FlipCard />
+      <PulseGlowCard />
     </div>
   );
 };
