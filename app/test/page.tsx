09dfd3922e412b07820b224964327dea8aee8bf9
Prohor722 <prohor.b722@gmail.com>
2025-09-21 01:12:26 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,13 +2,14 @@
 
 import FloatingCard from "../card/components/FloatingCard";
 import GlassmorphismProfileCard from "../card/components/GlassmorphismProfileCard";
+import GradientBorderProductCard from "../card/components/GradientBorderProductCard";
 import LikeButtonCard from "../card/components/LikeButtonCard";
 
 const page = () => {
 
   return (
     <div>
-      <GlassmorphismProfileCard />
+      <GradientBorderProductCard />
     </div>
   );
 };
