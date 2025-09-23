823e1731367bec845195cbe2906f343b419ff2b8
Prohor722 <prohor.b722@gmail.com>
2025-09-23 10:31:48 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,12 +2,13 @@
 
 import AnimatedGradientCard from "../card/components/AnimatedGradientCard";
 import D3TiltingCard from "../card/components/D3TiltingCard";
+import SocialMediaCard from "../card/components/SocialMediaCard";
 
 const page = () => {
 
   return (
     <div>
-      <AnimatedGradientCard />
+      <SocialMediaCard />
     </div>
   );
 };
