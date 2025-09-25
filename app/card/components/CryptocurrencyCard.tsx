8f4000144f12174bd8197b5fbded2706cc48e82f
Prohor722 <prohor.b722@gmail.com>
2025-09-26 01:02:18 +0600

    some issues fixed

--- app/card/components/CryptocurrencyCard.tsx
+++ app/card/components/CryptocurrencyCard.tsx
@@ -22,9 +22,9 @@ const CryptocurrencyCard = () => {
               viewBox="0 0 20 20"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
             +5.67%
