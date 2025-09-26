71277fcc42cb6f634abafd595dced2bc04fd2579
Prohor722 <prohor.b722@gmail.com>
2025-09-27 03:26:54 +0600

    some issues fixed

--- app/card/components/TeamMemberCard.tsx
+++ app/card/components/TeamMemberCard.tsx
@@ -38,9 +38,9 @@ const TeamMemberCard = () => {
           <button className="px-4 py-2 border-2 border-purple-500 text-purple-500 rounded-full hover:bg-purple-500 hover:text-white transition-all">
             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
