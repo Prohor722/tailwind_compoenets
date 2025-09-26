45fd518c913808f158524425296ddbde35ffe77a
Prohor722 <prohor.b722@gmail.com>
2025-09-27 03:29:43 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import TeamMemberCard from "../card/components/TeamMemberCard";
+import UsageInstructions from "../card/components/UsageInstructions";
 import AnimatedCards from "../test6";
 
 const page = () => {
 
   return (
     <div>
-      <TeamMemberCard />
+      <UsageInstructions />
     </div>
   );
 };
