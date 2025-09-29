21158506fd14ee1938f0db1ad40ee0f314c244f6
Prohor722 <prohor.b722@gmail.com>
2025-09-30 03:43:50 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,13 +3,14 @@
 import CodeExamplesCard from "../card/components/CodeExamplesCard";
 import D3FloatingCard from "../card/components/D3FloatingCard";
 import GlassmorphismCard from "../card/components/GlassmorphismCard";
+import MinimalElegantCard from "../card/components/MinimalElegantCard";
 import NeonCard from "../card/components/NeonCard";
 
 const page = () => {
 
   return (
     <div>
-      <D3FloatingCard />
+      <MinimalElegantCard />
     </div>
   );
 };
