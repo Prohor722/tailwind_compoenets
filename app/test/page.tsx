c4b5639ab7c124bbdb2943334738979cc5919baa
Prohor722 <prohor.b722@gmail.com>
2025-09-30 03:46:38 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import CodeExamplesCard from "../card/components/CodeExamplesCard";
-import D3FloatingCard from "../card/components/D3FloatingCard";
-import GlassmorphismCard from "../card/components/GlassmorphismCard";
-import MinimalElegantCard from "../card/components/MinimalElegantCard";
-import NeonCard from "../card/components/NeonCard";
+import GradientMeshCard from "../card/components/GradientMeshCard";
+
 
 const page = () => {
 
   return (
     <div>
-      <MinimalElegantCard />
+      <GradientMeshCard />
     </div>
   );
 };
