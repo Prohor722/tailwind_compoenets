9fe6e3450d644d60328655727e957e77946312ba
Prohor722 <prohor.b722@gmail.com>
2025-10-06 05:01:49 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,13 +3,14 @@
 import AnimatedIconsNavigation from "../navbar/components/AnimatedIconsNavigation";
 import EcommerceCategorie from "../navbar/components/EcommerceCategorie";
 import GlassmorphicFloatingParticles from "../navbar/components/GlassmorphicFloatingParticles";
+import LiquidMorphingEffect from "../navbar/components/LiquidMorphingEffect";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <AnimatedIconsNavigation />
+      <LiquidMorphingEffect />
     </div>
   );
 };
