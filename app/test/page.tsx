077d76b48aeffea2e5932fe34336aca324e42ccb
Prohor722 <prohor.b722@gmail.com>
2025-10-07 09:39:51 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import AnimatedIconsNavigation from "../navbar/components/AnimatedIconsNavigation";
-import EcommerceCategorie from "../navbar/components/EcommerceCategorie";
-import GlassmorphicFloatingParticles from "../navbar/components/GlassmorphicFloatingParticles";
-import LiquidMorphingEffect from "../navbar/components/LiquidMorphingEffect";
+import NeonGlowEffect from "../navbar/components/NeonGlowEffect";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <LiquidMorphingEffect />
+      <NeonGlowEffect />
     </div>
   );
 };
