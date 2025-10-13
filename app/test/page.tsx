ef20f7d5c2c80ae23253833b746d895824d8161b
Prohor722 <prohor.b722@gmail.com>
2025-10-13 09:23:06 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,12 +2,6 @@
 
 import AuroraBorealisEffect from "../navbar/components/AuroraBorealisEffect";
 import DarkModeToggle from "../navbar/components/DarkModeToggle";
-import DayNightToggle from "../navbar/components/DayNightToggle";
-import DepthEffect from "../navbar/components/DepthEffect";
-import GradientBorder from "../navbar/components/GradientBorder";
-import MinimalFloating from "../navbar/components/MinimalFloating";
-import PulseRadarEffect from "../navbar/components/PulseRadarEffect";
-import SplitDesign from "../navbar/components/SplitDesign";
 
 
 const page = () => {
