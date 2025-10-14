1cb53468dfb5fe26540049b43ddd36437de22d3c
Prohor722 <prohor.b722@gmail.com>
2025-10-15 01:16:47 +0600

    some issues fixed

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -5,12 +5,12 @@ import HoverableTable from "./components/HoverableTable";
 
 const table = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-10 flex flex-col items-center justify-center gap-10">
-      <h1 className="text-2xl font-bold text-blue-500">
+    <div className="min-h-screen bg-gray-100 p-6 flex flex-col items-center gap-10">
+      <h1 className="text-2xl mt-6 font-bold text-blue-500">
         Tailwind Table Showcase
       </h1>
 
-      <div className="flex flex-wrap justify-center gap-10">
+      <div className="flex flex-wrap justify-center gap-4">
 
         <BorderedTable />
 
