9321815107f2f46f7adca4b4638918ece4a7e80f
Prohor722 <prohor.b722@gmail.com>
2025-10-22 08:33:26 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,5 @@
 "use client";
-
-import CardStyle from "../table/components/CardStyle";
-import ColorfulHeaders from "../table/components/ColorfulHeaders";
-import CompactLeftAccent from "../table/components/CompactLeftAccent";
-import ExpandableRows from "../table/components/ExpandableRows";
 import Glassmorphism from "../table/components/Glassmorphism";
-import MinimalHoverEffect from "../table/components/MinimalHoverEffect";
-import ShadowedCards from "../table/components/ShadowedCards";
-import StatusIndicators from "../table/components/StatusIndicators";
-import StripedDarkAccent from "../table/components/StripedDarkAccent";
-import VerticalTimelineStyle from "../table/components/VerticalTimelineStyle";
-import WithSearchFilter from "../table/components/WithSearchFilter";
 
 
 const page = () => {
