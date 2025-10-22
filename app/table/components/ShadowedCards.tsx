e28b9e6591221bed65cc617cd075b5f64bc61ffb
Prohor722 <prohor.b722@gmail.com>
2025-10-22 08:32:38 +0600

    some issues fixed

--- app/table/components/ShadowedCards.tsx
+++ app/table/components/ShadowedCards.tsx
@@ -64,7 +64,7 @@ const ShadowedCards = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="max-w-lg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {data.map((item) => (
         <div
           key={item.id}
