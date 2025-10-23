fd1446d84e8716db9b68e94cca644394c0ced30d
Prohor722 <prohor.b722@gmail.com>
2025-10-24 00:50:54 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import CheckboxSelectionGradientHighlight from "../table/components/CheckboxSelectionGradientHighlight";
-import DualColumnHover from "../table/components/DualColumnHover";
-import Glassmorphism from "../table/components/Glassmorphism";
-import SparklineCharts from "../table/components/SparklineCharts";
 
 
 const page = () => {
