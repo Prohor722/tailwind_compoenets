07e17b709a3ba4d2ac345d8a0c9e8b34602a0af6
Prohor722 <prohor.b722@gmail.com>
2025-10-27 01:44:04 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,4 @@
 "use client";
-import AnimatedProgressRows from "../table/components/AnimatedProgressRows";
-import CheckboxSelectionGradientHighlight from "../table/components/CheckboxSelectionGradientHighlight";
-import KanbanBoardStyle from "../table/components/KanbanBoardStyle";
-import NestedDetails from "../table/components/NestedDetails";
-import SocialMediaFeedStyle from "../table/components/SocialMediaFeedStyle";
 import TimelineBadges from "../table/components/TimelineBadges";
 
 
