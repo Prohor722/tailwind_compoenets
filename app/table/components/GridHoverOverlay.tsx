bd6a2a46de60c70b6735a1aaa17acfc2e539e279
Prohor722 <prohor.b722@gmail.com>
2025-10-27 01:50:08 +0600

    some issues fixed

--- app/table/components/GridHoverOverlay.tsx
+++ app/table/components/GridHoverOverlay.tsx
@@ -57,7 +57,7 @@ const GridHoverOverlay = () => {
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="max-w-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {data.map((item) => (
         <div
           key={item.id}
