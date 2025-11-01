573522927da1a4acf0933e0a3088d9844417bf5f
Prohor722 <prohor.b722@gmail.com>
2025-11-01 13:19:37 +0600

    some issues fixed

--- app/table/components/SidebarActionsTable.tsx
+++ app/table/components/SidebarActionsTable.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { useState } from "react";
 
 const SidebarActionsTable = () => {
+  const [favorites, setFavorites] = useState(new Set());
   const data = [
     {
       id: 1,
