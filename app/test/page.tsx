3b6766b5fa35c852936f215c6963077bd62ded46
Prohor722 <prohor.b722@gmail.com>
2025-11-02 21:20:57 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import ColumnVisibilityToggle from "../table/components/ColumnVisibilityToggle";
+import StatusIndicatorRows from "../table/components/StatusIndicatorRows";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <ColumnVisibilityToggle />
+      <StatusIndicatorRows />
     </div>
   );
 };
