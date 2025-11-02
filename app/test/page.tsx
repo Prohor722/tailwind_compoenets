26b6c550adcb2817411181c87ca45aa0f7a004bd
Prohor722 <prohor.b722@gmail.com>
2025-11-02 21:31:29 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,13 @@
-"use client";
 import ColumnVisibilityToggle from "../table/components/ColumnVisibilityToggle";
 import StatusIndicatorRows from "../table/components/StatusIndicatorRows";
+import TrendVisualization from "../table/components/TrendVisualization";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <StatusIndicatorRows />
+      <TrendVisualization />
     </div>
   );
 };
