36c12eed2e879b04e7e88a5de62ea70a92366bdb
Prohor722 <prohor.b722@gmail.com>
2025-11-03 21:05:35 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,4 +1,5 @@
 import ColumnVisibilityToggle from "../table/components/ColumnVisibilityToggle";
+import ContactCardLayout from "../table/components/ContactCardLayout";
 import StatusIndicatorRows from "../table/components/StatusIndicatorRows";
 import TrendVisualization from "../table/components/TrendVisualization";
 
@@ -7,7 +8,7 @@ const page = () => {
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <TrendVisualization />
+      <ContactCardLayout />
     </div>
   );
 };
