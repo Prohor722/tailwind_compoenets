91ccd3877657eb2ad5e5980032a6dc71a4c26e12
Prohor722 <prohor.b722@gmail.com>
2025-11-03 21:12:51 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,6 @@
 import ColumnVisibilityToggle from "../table/components/ColumnVisibilityToggle";
 import ContactCardLayout from "../table/components/ContactCardLayout";
+import IconRichDataTable from "../table/components/IconRichDataTable";
 import StatusIndicatorRows from "../table/components/StatusIndicatorRows";
 import TrendVisualization from "../table/components/TrendVisualization";
 
@@ -8,7 +9,7 @@ const page = () => {
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <ContactCardLayout />
+      <IconRichDataTable />
     </div>
   );
 };
