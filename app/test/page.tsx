e996c76f798950df34b4cb4d206c8e14d2ec7d8a
Prohor722 <prohor.b722@gmail.com>
2025-11-04 22:53:27 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,13 @@
 import AnimatedBadgeTable from "../table/components/AnimatedBadgeTable";
 import IconRichDataTable from "../table/components/IconRichDataTable";
+import SalaryTransparencyTable from "../table/components/SalaryTransparencyTable";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <AnimatedBadgeTable />
+      <SalaryTransparencyTable />
     </div>
   );
 };
