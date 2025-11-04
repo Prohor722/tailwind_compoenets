642ebd5f403a188cbab5880f7537666d8b751a41
Prohor722 <prohor.b722@gmail.com>
2025-11-05 04:00:51 +0600

    some issues fixed

--- app/table/components/NestedHierarchyTable.tsx
+++ app/table/components/NestedHierarchyTable.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ChevronDown } from 'lucide-react';
 
 const NestedHierarchyTable = () => {
   const [expandedId, setExpandedId] = useState<number | null>(null);
