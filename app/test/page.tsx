37466fd4720941f9b2d9d3e091ec4c41f17c5d77
Prohor722 <prohor.b722@gmail.com>
2025-11-06 19:55:26 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,4 @@
 import BulkActionTable from "../table/components/BulkActionTable";
-import InlineEditTable from "../table/components/InlineEditTable";
-import NestedHierarchyTable from "../table/components/NestedHierarchyTable";
-import SalaryTransparencyTable from "../table/components/SalaryTransparencyTable";
 
 const page = () => {
 
