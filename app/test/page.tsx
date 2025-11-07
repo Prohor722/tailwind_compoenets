8a6f9f6390119687aa2aa37b7c55fd42c897b4fa
Prohor722 <prohor.b722@gmail.com>
2025-11-07 20:19:43 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,13 @@
 import BulkActionTable from "../table/components/BulkActionTable";
 import MultiStatRowTable from "../table/components/MultiStatRowTable";
+import TimelineStatusTable from "../table/components/TimelineStatusTable";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <MultiStatRowTable />
+      <TimelineStatusTable />
     </div>
   );
 };
