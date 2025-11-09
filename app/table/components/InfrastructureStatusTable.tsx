9746464689cbd8d3acfa119a3df12c38186880c3
Prohor722 <prohor.b722@gmail.com>
2025-11-09 21:30:38 +0600

    some issues fixed

--- app/table/components/InfrastructureStatusTable.tsx
+++ app/table/components/InfrastructureStatusTable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ChevronRight, Share2, Link2, Copy, Check, AlertCircle, CheckCircle2, GitPullRequest, Code2, Database, Cloud, Server, Wifi } from 'lucide-react';
+
 
 const InfrastructureStatusTable = () => {
   
