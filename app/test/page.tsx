f9aeb90124f4382a95c193f4ce3aeaf7e9b29e8f
Prohor722 <prohor.b722@gmail.com>
2025-11-10 23:07:29 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,13 @@
 import CompactActionMenu from "../table/components/CompactActionMenu";
 import PullRequestStyleTable from "../table/components/PullRequestStyleTable";
+import TechnologyStackDisplay from "../table/components/TechnologyStackDisplay";
 
 const page = () => {
 
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <CompactActionMenu />
+      <TechnologyStackDisplay />
     </div>
   );
 };
