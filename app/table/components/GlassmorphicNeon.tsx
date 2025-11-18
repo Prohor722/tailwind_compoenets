735c9637c9c242f7a10169e1bb4293bc36449487
Prohor722 <prohor.b722@gmail.com>
2025-11-18 23:47:11 +0600

    some issues fixed

--- app/table/components/GlassmorphicNeon.tsx
+++ app/table/components/GlassmorphicNeon.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import { useState } from "react";
 
 const GlassmorphicNeon = () => {
   const [hoveredId, setHoveredId] = useState<number | null>(null);
