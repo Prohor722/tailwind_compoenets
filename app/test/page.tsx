48c2161c4dab3f786e7899234b0d039080736d5e
Prohor722 <prohor.b722@gmail.com>
2025-11-25 23:31:29 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,5 @@
 
-import CardStackEffect from "../table/components/CardStackEffect";
-import CellularMatrix from "../table/components/CellularMatrix";
-import GlassmorphicNeon from "../table/components/GlassmorphicNeon";
 import HolographicGridTable from "../table/components/HolographicGridTable";
-import KnowledgeBaseArticlesTable from "../table/components/KnowledgeBaseArticlesTable";
 
 const page = () => {
 
