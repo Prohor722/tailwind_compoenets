f8fb5c467dd4e7670c4e66bc9248b84fc6cdece4
Prohor722 <prohor.b722@gmail.com>
2025-11-27 23:27:09 +0600

    some issues fixed

---
+++ app/table/components/GradientBarChartHybridTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GradientBarChartHybridTable = () => {
+  return (
+    <div>GradientBarChartHybridTable</div>
+  )
+}
+
+export default GradientBarChartHybridTable
