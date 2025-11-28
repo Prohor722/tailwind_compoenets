366c40a6d67f1d7c70aed43bb909328b698bab83
Prohor722 <prohor.b722@gmail.com>
2025-11-28 23:04:58 +0600

    some issues fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,4 @@
-
-import FloatingRowSelectionTable from "../table/components/FloatingRowSelectionTable";
-import GradientBarChartHybridTable from "../table/components/GradientBarChartHybridTable";
-import HolographicGridTable from "../table/components/HolographicGridTable";
 import Isometric3DCardsTable from "../table/components/Isometric3DCardsTable";
-import MinimalistBlocksTable from "../table/components/MinimalistBlocksTable";
-import TimelineViewTable from "../table/components/TimelineViewTable";
 
 const page = () => {
 
