a706830acca426ab75564a813d0c5d6b58730116
Prohor722 <prohor.b722@gmail.com>
2025-12-01 23:27:51 +0600

    test done

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,6 @@
 import BubbleNetworkTable from "../table/components/BubbleNetworkTable";
 import FloatingRowSelection from "../table/components/FloatingRowSelection";
+import GradientBarChartHybrid from "../table/components/GradientBarChartHybrid";
 import Isometric3DCardsTable from "../table/components/Isometric3DCardsTable";
 import MinimalDotsConnectionTable from "../table/components/MinimalDotsConnectionTable";
 import StackedRowsTable from "../table/components/StackedRowsTable";
@@ -9,7 +10,7 @@ const page = () => {
   return (
     <div>
       {/* <EcommerceCategorie /> */}
-      <FloatingRowSelection />
+      <GradientBarChartHybrid />
     </div>
   );
 };
